f4b7465189697b7c166c9537d6ff29fb1ac187db
Frederick R. Cook <frcook.6060@gmail.com>
2025-01-21 22:06:45 -0600

    Working on an example

---
+++ example/main.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('index', {})
+});
+
+
+app.listen(port, () => {
+    console.log(`Listening on port http://localhost:${port}`)
+})
